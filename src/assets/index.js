022f317f0995a03d00e761058d07926732df0ac1
Sahil  Madavi <97336225+SAHIL-KUN@users.noreply.github.com>
2025-03-16 15:43:20 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import unrealengin from "./tech/unrealengin.png";
+import figma from "./tech/figma.png";
+import cpp from "./tech/cpp.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import flutter from "./tech/flutter.png";
+import tailwind from "./tech/tailwind.png";
+import python from "./tech/python.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  unrealengin,
+  figma,
+  cpp,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  flutter,
+  tailwind,
+  python,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
